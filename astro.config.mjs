7d54601c89deef5ad3339c29d61804af39f116c4
braidata <ibraida@braidata.com>
2023-10-17 01:13:20 +0000

    grandes cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [    tailwind({
     // Example: Provide a custom path to a Tailwind config file
     configFile: './tailwind.config.cjs',
   }), react({
-    experimentalReactChildren: true,
+    
   }),]
 });
